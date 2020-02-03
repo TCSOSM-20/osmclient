09bb6134d43ca1449fbc2ccf7430de0da4f1e109
tierno <alfonso.tiernosepulveda@telefonica.com>
2020-02-03 14:48:59 +0000

    fix 1006 command wim-show tries to load json twice
    Change-Id: Ie444c6856c2541d207432ca98d0cbedccd91c192
    Signed-off-by: tierno <alfonso.tiernosepulveda@telefonica.com>

--- osmclient/sol005/wim.py
+++ osmclient/sol005/wim.py
@@ -235,7 +235,7 @@ class Wim(object):
                 resp =  json.loads(resp)
             if not resp or '_id' not in resp:
                 raise ClientException('failed to get wim info: '.format(resp))
-            return json.loads(resp)
+            return resp
         except NotFound:
             raise NotFound("wim '{}' not found".format(name))
 
